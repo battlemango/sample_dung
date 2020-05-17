ff239ff73ec7a7640c5951dd05ac9a14ccfd6515
song <togetheri86@gmail.com>
2020-05-17 13:20:05 +0900

    add sample2 init project
    add sample2 init project

---
+++ sample_dung_2/Assets/main/src/Ddong.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ddong : MonoBehaviour
+{
+    private float GRAVITY = 9.8f;
+
+    private float mVelocity = 0f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 current = this.transform.position;
+
+        mVelocity += GRAVITY * Time.deltaTime;
+
+        current.y -= mVelocity * Time.deltaTime;
+        this.transform.position = current;
+
+        
+    }
+
+}
